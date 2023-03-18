33937e2b42784a60832b52e46332b6fb12289d02
Alexis <bzdalexis@gmail.com>
2023-03-18 15:14:29 +0100

    add retrofit interface to call API

---
+++ app/src/main/java/com/alexisboiz/covoitapp/service/CarpoolService.java
@@ -0,0 +1,14 @@
+package com.alexisboiz.covoitapp.service;
+
+import com.alexisboiz.covoitapp.model.CarpoolAreaData;
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.QueryMap;
+
+public interface CarpoolService {
+    @GET("api/records/1.0/search/?")
+    Call<CarpoolAreaData> getCarpoolArea(@QueryMap Map<String,String> dataset);
+}
