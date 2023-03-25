736145034016188e5277b2f0128fa545b4138d19
Alexis <bzdalexis@gmail.com>
2023-03-25 15:59:46 +0100

    update data model

--- app/src/main/java/com/alexisboiz/covoitapp/model/CarpoolAreaData.java
+++ app/src/main/java/com/alexisboiz/covoitapp/model/CarpoolAreaData.java
@@ -5,6 +5,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.ObservableArrayList;
 
 import com.alexisboiz.covoitapp.model.API_Data.Fields;
 import com.alexisboiz.covoitapp.model.API_Data.Parameters;
@@ -13,7 +14,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class CarpoolAreaData implements Parcelable {
 
     @SerializedName("nhits")
@@ -24,30 +29,7 @@ public final class CarpoolAreaData implements Parcelable {
     private Parameters parameters;
     @SerializedName("records")
     @Expose
-    private List<Record> records;
-
-    private static CarpoolAreaData  instance;
-
-    protected CarpoolAreaData(Parcel in) {
-        if (in.readByte() == 0) {
-            nhits = null;
-        } else {
-            nhits = in.readInt();
-        }
-        parameters = in.readParcelable(Parameters.class.getClassLoader());
-        records = in.createTypedArrayList(Record.CREATOR);
-    }
-
-    private CarpoolAreaData(){
-
-    }
-
-    public static CarpoolAreaData getInstance(){
-        if(instance == null){
-            instance = new CarpoolAreaData();
-        }
-        return instance;
-    }
+    private ObservableArrayList<Record> records;
 
     public static final Creator<CarpoolAreaData> CREATOR = new Creator<CarpoolAreaData>() {
         @Override
@@ -60,6 +42,14 @@ public final class CarpoolAreaData implements Parcelable {
             return new CarpoolAreaData[size];
         }
     };
+
+    public CarpoolAreaData(){
+
+    }
+
+    protected CarpoolAreaData(Parcel in) {
+
+    }
 
     public Integer getNhits() {
         return nhits;
@@ -77,20 +67,28 @@ public final class CarpoolAreaData implements Parcelable {
         this.parameters = parameters;
     }
 
-    public List<Record> getRecords() {
+    public ObservableArrayList<Record> getRecords() {
         return records;
     }
 
-    public void setRecords(List<Record> records) {
+    public void setRecords(ObservableArrayList<Record> records) {
         this.records = records;
     }
 
-    public void append(CarpoolAreaData data){
-        List<Record> tempRecords = this.getRecords();
-        tempRecords.addAll(data.getRecords());
-        this.setRecords(tempRecords);
-    }
+    public void append(CarpoolAreaData data, boolean appendAtBegin){
+        if(appendAtBegin){
+            ObservableArrayList<Record> dataRecord = data.getRecords();
+            dataRecord.addAll(this.getRecords());
+            this.records.clear();
+            this.setRecords(dataRecord);
+        }else{
+            ObservableArrayList<Record> tempRecords = this.getRecords();
+            tempRecords.addAll(data.getRecords());
+            //this.records.clear();
+            this.setRecords(tempRecords);
+        }
 
+    }
     public Fields getFieldsWithCoordinate(LatLng coordinate){
         for(Record record : records){
             Double lat = record.getFields().getCoordonnees().get(0);
@@ -119,5 +117,68 @@ public final class CarpoolAreaData implements Parcelable {
         }
     }
 
+    public List<Record> find(CharSequence charSequence){
+        List<Record> goodRecord = new ArrayList<>();
+        Pattern pattern = Pattern.compile("[0-9]");
+        Matcher matcher = pattern.matcher(charSequence);
+        boolean isNumber = matcher.find();
+        for(Record record : records){
+            if(isNumber){
+                String zip = record.getFields().getCodePostal();
+                if(zip.contains(charSequence)){
+                    goodRecord.add(record);
+                }
+            }else{
+                String name = record.getFields().getNomDuLieu();
+                if(name.contains(charSequence)){
+                    goodRecord.add(record);
+                }
+            }
+        }
 
+
+        return goodRecord;
+    }
+    public ObservableArrayList<Record> findWithRecordId(List<String> recordIdList){
+        ObservableArrayList<Record> goodRecords = new ObservableArrayList<>();
+        for(String recordId : recordIdList){
+            for(Record record : records){
+                if(recordId.equals(record.getRecordid())){
+                    goodRecords.add(record);
+                }
+            }
+        }
+        return goodRecords;
+    }
+    public Record findWithRecordId(String recordId){
+        Record goodRecords = null;
+            for(Record record : records){
+                if(recordId.equals(record.getRecordid())){
+                    goodRecords = record;
+                }
+            }
+        return goodRecords;
+    }
+
+
+    public void moveAtTop(String recordId){
+        Record givedRecord = findWithRecordId(recordId);
+        records.add(0, givedRecord);
+    }
+
+    public void deleteDuplicate(){
+        for (Record record : records){
+            for (Record possibleDuplicateRecord : records){
+                if(record.getRecordid().equals(possibleDuplicateRecord.getRecordid())){
+                    records.remove(record);
+                }
+            }
+        }
+    }
+
+    public void moveToFirst(Record record){
+        int index = records.indexOf(record);
+        records.remove(index);
+        records.add(0, record);
+    }
 }
