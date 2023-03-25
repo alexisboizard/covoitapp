9050db24027e8658c9897597caca91aded0dae15
Alexis <bzdalexis@gmail.com>
2023-03-25 15:56:54 +0100

    update the splashscreen with gif

--- app/src/main/java/com/alexisboiz/covoitapp/SplashScreen.java
+++ app/src/main/java/com/alexisboiz/covoitapp/SplashScreen.java
@@ -6,9 +6,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,10 +21,6 @@ public class SplashScreen extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
-
-        ImageView backgroundImage = findViewById(R.id.SplashScreenImage);
-        Animation slideAnimation = AnimationUtils.loadAnimation(this,R.anim.slide_slide);
-        backgroundImage.startAnimation(slideAnimation);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             new Handler().postDelayed(new Runnable() {
