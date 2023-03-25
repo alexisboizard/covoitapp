696000364b80b7de3edacda8e51f72dca8ca0af9
Alexis <bzdalexis@gmail.com>
2023-03-25 15:58:31 +0100

    refactor params name for more clarity

--- app/src/main/java/com/alexisboiz/covoitapp/manager/CarpoolAreaDataManagerCallback.java
+++ app/src/main/java/com/alexisboiz/covoitapp/manager/CarpoolAreaDataManagerCallback.java
@@ -3,6 +3,6 @@ package com.alexisboiz.covoitapp.manager;
 import com.alexisboiz.covoitapp.model.CarpoolAreaData;
 
 public interface CarpoolAreaDataManagerCallback {
-    void getCarpoolAreaResponseSuccess(CarpoolAreaData carpoolAreaData);
+    void getCarpoolAreaResponseSuccess(CarpoolAreaData data);
     void getCarpoolAreaResponseError(String error);
 }
